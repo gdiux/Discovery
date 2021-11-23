66ff3373ceccb01767ec626ccecec242a3318bb2
discoverysystemspos <elkindanielcastillo@gmail.com>
2021-11-23 01:25:35 -0500

    Discovery

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+// COMPONENTS
+import { HomeComponent } from './home/home.component';
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
+import { SearchComponent } from './search/search.component';
+import { ProductComponent } from './product/product.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent, data:{ titulo: 'Inicio'} },
+  { path: 'search', component: SearchComponent, data:{ titulo: 'Buscar'} },
+  { path: 'product', component: ProductComponent, data:{ titulo: 'Producto'} },
+  { path: '**', component: NopagefoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
+import { SearchComponent } from './search/search.component';
+import { ProductComponent } from './product/product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    NopagefoundComponent,
+    SearchComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
