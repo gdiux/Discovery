a3feced39af23db55690f343068828cb49721993
discoverysystemspos <elkindanielcastillo@gmail.com>
2021-11-24 01:29:07 -0500

    Casi terminada

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+// SERIVES
+import { SearchService } from '../services/search.service';
+import { DepartmentService } from '../services/department.service';
+
+// MODELS
+import { Product } from '../models/product.model';
+import { Department } from '../models/department.model';
 
 @Component({
   selector: 'app-search',
@@ -8,9 +17,94 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  public desde: number = 0;
+  public hasta: number = 10;
+  public tipo: string = '';
+  public termino: string = '';
+
+  constructor(  private router: Router,
+                private activatedRoute: ActivatedRoute,
+                private searchService: SearchService,
+                private departmentService: DepartmentService) { }
 
   ngOnInit(): void {
+
+    this.activatedRoute.params.subscribe( ({tipo, termino}) => {
+
+      this.tipo = tipo;
+      this.termino = termino;
+      
+      this.buscador();
+      
+    });
+
+    this.cargarDepartamento();
+
+  }
+
+  /** ================================================================
+   *   BUSCAR
+  ==================================================================== */
+  public productos: Product[] = [];
+  public total: number = 0;
+
+  public btnAtras: string = 'false';
+  public btnAdelante: string = 'true';
+
+  buscador(){
+
+    this.searchService.search(this.tipo, this.termino, this.desde, this.hasta)
+        .subscribe( ({resultados}) => {
+          
+          this.productos = resultados;
+          this.total = resultados.length;          
+          
+        });
+  }
+
+  /** ================================================================
+   *  CAMBIAR PAGINA
+  ==================================================================== */
+  cambiarPagina(valor: number){
+
+    this.desde += valor;    
+
+    if (this.desde < 0) {
+      this.desde = 0;
+    }
+    this.buscador();
+
+  }
+
+  /** ================================================================
+   *  CARGAR DEPARTAMENTOS
+  ==================================================================== */
+  public departamentos: Department[] = [];
+  cargarDepartamento(){
+
+    this.departmentService.loadDepartment( this.desde, this.hasta )
+        .subscribe(({ total, departments }) =>{
+
+          this.departamentos = departments;
+
+        }, (err) => {  }
+        
+        )
+
+  }
+
+  /** ================================================================
+   *  BUSCAR PRODUCTOS
+  ==================================================================== */
+  buscarProductos(termino:string){
+
+    if (termino.length === 0) {
+      return;
+    }  
+
+    // INGRESAR
+    this.router.navigateByUrl(`/search/producto/${termino}`);
+
   }
 
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  base_url: 'http://localhost:3000/api',
+  base_url: 'https://discoverysystemspos.com/api',
   local_url: 'https://discoverysystemspos.com',
   admin_url: 'https://admin.discoverysystemspos.com/api'
 
